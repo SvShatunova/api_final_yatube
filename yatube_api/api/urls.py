8797bd7b24efff008142b8184303e18b764e141d
svshatunova <svshatun0va@yandex.ru>
2023-06-22 01:35:00 +0300

    homework

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,30 +1,20 @@
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
-# Регистрация представлений связанных с моделью Post
-post_router = DefaultRouter()
-post_router.register('', PostViewSet, basename='post')
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet, basename='followers')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments',
+)
 
-# Регистрация представлений связанных с моделью Comment
-comment_router = DefaultRouter()
-comment_router.register('', CommentViewSet, basename='comment')
-
-# Регистрация представления для модели Group
-group_router = DefaultRouter()
-group_router.register('', GroupViewSet, basename='group')
-
-# Регистрация представления для модели Follow
-follow_router = DefaultRouter()
-follow_router.register('', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('v1/posts/', include(post_router.urls)),
-    path('v1/groups/', include(group_router.urls)),
-    path('v1/posts/<int:post_id>/comments/', include(comment_router.urls)),
-    path('v1/follow/', include(follow_router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token, name='url_api_token'),
+    path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls.jwt')),
 ]
